f0df6cea751b2c04e139f8c0ba9082a4cb52663f
Llennoc54 <98364642+Llennoc54@users.noreply.github.com>
2026-03-06 00:21:43 -0600

    More URLS

--- client/src/BookListings.jsx
+++ client/src/BookListings.jsx
@@ -23,7 +23,7 @@ function BookListings() {
   useEffect(() => {
     const fetchListings = async () => {
       try {
-        const response = await fetch("p2p.up.railway.app"); 
+        const response = await fetch("diplomatic-playfulness.up.railway.app"); 
         if (!response.ok) throw new Error("Failed to fetch listings");
         
         const data = await response.json();
