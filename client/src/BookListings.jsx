2eb1cbe7332965f575753ec66d1d76d6aef1dc75
Llennoc54 <98364642+Llennoc54@users.noreply.github.com>
2026-03-06 00:19:11 -0600

    Still trying urls hoping it fixes itself

--- client/src/BookListings.jsx
+++ client/src/BookListings.jsx
@@ -23,7 +23,7 @@ function BookListings() {
   useEffect(() => {
     const fetchListings = async () => {
       try {
-        const response = await fetch("https://peer-to-peer-book-exchange2.vercel.app"); 
+        const response = await fetch("peer-to-peer-book-exchange2.up.railway.app"); 
         if (!response.ok) throw new Error("Failed to fetch listings");
         
         const data = await response.json();
